e71fd325e704a5b40ea4d0b0f23031fd2174f573
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-18 19:40:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,6 +81,10 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+        },
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
         }
      }
 
@@ -111,7 +115,11 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
+<<<<<<< HEAD
      programmer_hash
+=======
+     
+>>>>>>> 70a73091c1ee510b9a8ee4f0c39807542971fe9d
 end
 
 def adding_to_dennis
